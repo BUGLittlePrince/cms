33e848326a4a018295dc9f2dfe122b10e992e885
BUGLittlePrince <m18209581712_@163.com>
2021-09-01 09:33:32 +0800

    build(project build): global,router,store,service build

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import { globalRegister } from './global'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// registerApp(app)
+app.use(globalRegister)
+app.use(router)
+app.use(store)
+
+app.mount('#app')
---
+++ src/view/login/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>login : {{ $store.state.name }}</h2>
+    <el-button type="success">登录</el-button>
+    <el-button type="primary">登录</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {},
+  name: 'Login'
+})
+</script>
+
+<style scoped></style>
